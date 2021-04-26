3a7e4e81fcc6bd7216a4b7cea6ffa705024d06cd
Leonardo Meister <leozinhomm1@gmail.com>
2021-04-26 15:52:29 -0300

    Atualizacao Objetos Chamados e Equipamentos

--- Chamados.cs
+++ Chamados.cs
@@ -8,5 +8,45 @@ namespace GestaoEquipamentosOO
 {
     class Chamados
     {
+
+        private string titulo;
+        private string descricao;
+        private Equipamentos equipamento;
+        private DateTime dataAbertura;
+        private DateTime dataAtualizacao;
+
+        public string Titulo
+        {
+            get
+            {
+                return titulo;
+            }
+        }
+        public Equipamentos Equipamento
+        {
+            get
+            {
+                return equipamento;
+            }
+        }
+        public DateTime DataAbertura
+        {
+            get
+            {
+                return dataAbertura;
+            }
+        }
+
+        public string Descricao { get => descricao; set => descricao = value; }
+        public DateTime DataAtualizacao { get => dataAtualizacao; set => dataAtualizacao = value; }
+
+        public Chamados(string titulo, string descricao, DateTime dataAbertura, Equipamentos equipamento)
+        {
+            this.titulo = titulo;
+            this.descricao = descricao;
+            this.dataAbertura = dataAbertura;
+            this.equipamento = equipamento;
+            this.dataAtualizacao = dataAbertura;
+        }
     }
 }
--- Equipamentos.cs
+++ Equipamentos.cs
@@ -8,5 +8,42 @@ namespace GestaoEquipamentosOO
 {
     class Equipamentos
     {
+
+        private float preco;
+        private string nome;
+        private int serie;
+        private string fabricante;
+        private int id;
+        private DateTime dataFabricacao;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+        public DateTime DataFabricacao
+        {
+            get
+            {
+                return dataFabricacao;
+            }
+        }
+
+        public float Preco { get => preco; set => preco = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public int Serie { get => serie; set => serie = value; }
+        public string Fabricante { get => fabricante; set => fabricante = value; }
+
+        public Equipamentos(float preco, string nome, int serie, DateTime data, string fabricante)
+        {
+            this.Preco = preco;
+            this.Nome = nome;
+            this.Serie = serie;
+            this.dataFabricacao = data;
+            this.Fabricante = fabricante;
+        }
+
     }
 }
